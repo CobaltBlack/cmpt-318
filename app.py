7120464bb3784ca7957c59fb931aaa55644f1a64
Josh McManus <alex.joshua.mcmanus@gmail.com>
2017-10-29 15:14:38 -0700

    Adds output from API calls as JSON
    Also adds two functions for those API calls if they need to be made in the future

--- app.py
+++ app.py
@@ -5,7 +5,6 @@ Copyright (c) 2017 Joshua McManus, Eric Liu
 This applications and its creators have no affiliation with Valve or Steam.
 
 """
-
 import json
 import requests
 
@@ -14,19 +13,36 @@ API_KEY_LENGTH = 32
 
 STEAM_API_DOMAIN = 'http://api.steampowered.com/'
 
+# Makes a call to the Steam API to retrieve all Steam apps
+#  and saves it to a json file
+def FetchAppList():
+    req_url = (
+            STEAM_API_DOMAIN + 
+            'ISteamApps/GetAppList/v0002'
+            )
+    steamapps = requests.get(req_url)
+    with open('./apps.json', 'w') as f:
+        json.dump(steamapps.json(), f)
+        f.close
+
+def FetchSupportedApiCalls(API_KEY):
+    req_url = (
+           STEAM_API_DOMAIN + 
+           'ISteamWebAPIUtil/GetSupportedAPIList/v0001'
+           )
+    apis = requests.get(req_url, params={'key': API_KEY})
+    with open('./apis.json', 'w') as f:
+        json.dump(apis.json(), f)
+        f.close
+
 def main():
     # Fetch the API key for Steam from the root directory
     apiFile = open(API_FILE, 'r')
     API_KEY = apiFile.read().strip()
     assert len(API_KEY) == API_KEY_LENGTH
 
-    supported_apis = (
-            STEAM_API_DOMAIN + 
-            'ISteamWebAPIUtil/GetSupportedAPIList/v0001'
-            )
-    resp = requests.get(supported_apis, params={'key': API_KEY})
-    
-    API_CALLS = resp.json()
+    #FetchAppList();
+    #FetchSupportedApiCalls(API_KEY)
 
 if __name__ == "__main__":
     main()
