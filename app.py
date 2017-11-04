209dcc3da6b2c484d442987948d4655958fce9f6
Josh McManus <alex.joshua.mcmanus@gmail.com>
2017-11-03 18:44:40 -0700

    Changes project to crime statistics
    Unless Lord GabeN reaches down from the heavens and grants us his
    blessing, we shall have to revert to a life of crime (analysis).
    
    This just removes all of the data we don't need and begins by parsing
    the full crime dataset to give us the data we need.

--- app.py
+++ app.py
@@ -5,51 +5,26 @@ This applications and its creators have no affiliation with Valve or Steam.
 
 """
 
-from res.globvars import APPLIST
+#from res.globvars import APPLIST
+from CleanCrimeData import CleanCrimeData
+from res.globvars import RESOURCE_FILES_PATH
 
-import json
-import requests
+import os
+import sys
 import pandas as pd
 
-API_FILE = './api-key.txt'
-API_KEY_LENGTH = 32
-
-STEAM_API_DOMAIN = 'http://api.steampowered.com/'
-
-# Makes a call to the Steam API to retrieve all Steam apps
-#  and saves it to a json file
-def FetchAppList():
-    req_url = (
-            STEAM_API_DOMAIN + 
-            'ISteamApps/GetAppList/v0002'
-            )
-    steamapps = requests.get(req_url)
-    with open('./apps.json', 'w') as f:
-        json.dump(steamapps.json(), f)
-        f.close
-
-def FetchSupportedApiCalls(API_KEY):
-    req_url = (
-           STEAM_API_DOMAIN + 
-           'ISteamWebAPIUtil/GetSupportedAPIList/v0001'
-           )
-    apis = requests.get(req_url, params={'key': API_KEY})
-    with open('./apis.json', 'w') as f:
-        json.dump(apis.json(), f)
-        f.close
+CRIME_FILE = RESOURCE_FILES_PATH + "crime_data.csv"
 
 def main():
-    # Fetch the API key for Steam from the root directory
-    apiFile = open(API_FILE, 'r')
-    API_KEY = apiFile.read().strip()
-    assert len(API_KEY) == API_KEY_LENGTH
-
-    #FetchAppList();
-    #FetchSupportedApiCalls(API_KEY)
+    if not os.path.isfile(CRIME_FILE):
+        CleanCrimeData(RESOURCE_FILES_PATH + 'crime_csv_all_years.csv', CRIME_FILE)
+    try:
+        crime_data = pd.read_csv(CRIME_FILE)
+    except Exception as e:
+        print(e)
+        sys.exit(1)
     
-    appids = pd.io.json.json_normalize(APPLIST['applist']['apps'])
-    print(appids)
-
-
+    # Do the analysis...
+    
 if __name__ == "__main__":
     main()
--- res/__init__.py
+++ res/__init__.py
@@ -1,10 +1,7 @@
 """
 
 Copyright (c) 2017 Joshua McManus, Eric Liu
-This applications and its creators have no affiliation with Valve or Steam.
 
 """
 
 from . import globvars
-
-globvars.init()
